59834b6a2dbf3ce2455e898817d33311a74657db
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2019-09-07 00:04:27 +0200

    add test to vcs

---
+++ test/src/Main.purs
@@ -0,0 +1,23 @@
+module Main where
+
+-- import Data.Unit
+-- import Data.Show
+-- import Data.Semigroup ((<>))
+-- import Data.Semiring ((+), (*))
+-- import Control.Semigroupoid ((>>>))
+-- import Data.Function (const)
+-- g x | true = 1+2
+import Prelude
+import Effect.Console (log)
+
+-- main x = (_ + 1) >>> (_ * 8983)
+
+-- f = [12, 2,3] <>[2]
+
+-- fib 0 = 1
+-- fib n = n* fib n
+
+main = do
+  log "asd"
+  log "😘kkk"
+  pure 1
